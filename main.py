a258fd295c0f365b94bb917939570093e2444157
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-02-19 14:38:34 -0600

    test

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ import auth
 import bot
 from multiprocessing import Process, freeze_support
 
-if __name__ == '__main__':
-    # auth.authenticate()
-    bot.startBot()
+import hikari
+import lightbulb
+import os
+import dotenv
+import auth
 
-print("hi")
+def startBot():
+    dotenv.load_dotenv()
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    print("THIS IS THE TOKEN: " + BOT_TOKEN)
+
+    bot = lightbulb.BotApp(token=BOT_TOKEN)
+
+    @bot.command
+    @lightbulb.command('open', 'opens auth` url')
+    @lightbulb.implements(lightbulb.SlashCommand)
+    async def ping(ctx):
+        print(auth.getURL())
+        await ctx.respond (auth.getURL())
+
+    bot.run()
+
+startBot()
+# if __name__ == '__main__':
+#     # auth.authenticate()
+#     bot.startBot()
+
+# print("hi")
