6a2cb7f7473ea12d5eb25af2b10c5a5a2071f68a
Devin Liu <devin@Devins-MacBook-Pro.local>
2023-02-18 22:06:22 -0600

    print

--- bot.py
+++ bot.py
@@ -7,11 +7,12 @@ import auth
 def startBot():
     dotenv.load_dotenv()
     BOT_TOKEN = os.environ["BOT_TOKEN"]
+    print("THIS IS THE TOKEN: " + BOT_TOKEN)
 
     bot = lightbulb.BotApp(token=BOT_TOKEN)
 
     @bot.command
-    @lightbulb.command('open', 'opens auth url')
+    @lightbulb.command('open', 'opens auth` url')
     @lightbulb.implements(lightbulb.SlashCommand)
     async def ping(ctx):
         print(auth.getURL())
